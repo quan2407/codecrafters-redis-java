26fa18ca3190ee6f186976b42a031000843a00d2
quan2407 <quannm24072003@gmail.com>
2026-04-01 16:39:13 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,39 +7,56 @@ import java.net.Socket;
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
+
+
+      /*
+      * Socket là 1 file giúp máy tính có thể đẩy dữ liệu ra phía card mạng
+      * Phân tích:
+      * Về mặt phần cứng, CPU và RAM chỉ có thể nói chuyện với các thiết bị ngoại vi thông
+      * qua các chuẩn giao tiếp. Card mạng (NIC -Network Interface Card) là thiết bị như vậy
+      * ứng dụng java không được phép điều khển trực tiếp card mạng
+      * phải thông qua kernel( nhân hệ điều hành)
+      * socket chính là tay nắm cửa mà kernel đưa cho. khi đẩy dữ liệu vào socket, kernel sẽ tiếp quản
+      * chia nhỏ dữ liệu thành các gói tin(packets), thêm địa chỉ ip/mac và ra lệnh cho card phát sóng điện
+      * tu (wifi) hoặc xung điện (dây cáp)
+      * */
     System.out.println("Logs from your program will appear here!");
+    int port = 6379;
+    try (ServerSocket serverSocket = new ServerSocket(port)){
+        serverSocket.setReuseAddress(true);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("New user is coming: " + clientSocket.getRemoteSocketAddress());
 
-    //  Uncomment the code below to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          InputStream input = clientSocket.getInputStream();
-          OutputStream output = clientSocket.getOutputStream();
+            final Socket finalClientSocket = clientSocket;
 
-          byte[] buffer = new byte[1024];
-          int bytesRead;
+            Thread thread = new Thread(() -> {
+                try (Socket socket = finalClientSocket){
+                    /*
+                    * Socket khi khởi tại sẽ đi kèm 2 vùng nhớ như ở dưới
+                    * */
+                    // đây là vùng nhớ receive buffer: nhận dữ liệu từ mạng gửi tới
+                    InputStream input = socket.getInputStream();
+                    // đây là vùng nhớ send buffer, dữ liệu được đẩy vào đây, os sẽ quyết định khi nào
+                    // chia nhỏ vùng nhớ này thành các TCP Segments dể gửi đi
+                    OutputStream output = socket.getOutputStream();
+                    // đây là thùng hứng dữ liệu được lấy về vùng input
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
 
-          while ((bytesRead = input.read(buffer)) != -1){
-              output.write("+PONG\r\n".getBytes());
-              output.flush();
-          }
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
+                    while ((bytesRead = input.read(buffer)) != -1){
+                        output.write("+PONG\r\n".getBytes());
+                        // yêu cầu gửi luôn dữ liệu không đợi đổ dữ liệu khác đầy rồi mới gửi
+                        output.flush();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            thread.start();
         }
+    } catch (IOException e) {
+        System.err.println("ServerSocket error: " + e.getMessage());
+    }
   }
 }
